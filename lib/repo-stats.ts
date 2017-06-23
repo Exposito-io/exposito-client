901b37f18961128a18c84e440c36f1b23bdbc983
Mathew <mathew.corm@gmail.com>
2017-06-23 11:23:26 -0400

    repo-stats

---
+++ lib/repo-stats.ts
@@ -0,0 +1,50 @@
+import 'isomorphic-fetch'
+import { ExpositoClientOptions } from './exposito-client-options'
+import * as Rest from 'fetch-on-rest'
+import config from '../config'
+import { SendOptions, GetWalletOptions, CreatePaymentRequest, RepoParams, RepoStats } from 'models'
+
+
+
+
+export class RepoStatsProvider {
+
+    constructor(opts: ExpositoClientOptions = {}) {
+        this.version = opts.version || config.version
+        this.url = opts.url || config.url
+        this.api = Rest(`${this.url}/${this.version}`)
+    }
+
+
+    async get(params: RepoParams): Promise<RepoStats> {
+        try {
+            let result = await this.api.get(`repo-stats`, params)
+            if (result.error) {
+                throw('Error')
+            }
+            else
+                return result
+        } catch (e) {
+            throw(e)
+        }
+    }
+
+    async getMultiple(params: RepoParams[]): Promise<RepoStats> {
+        try {
+            let result = await this.api.get(`repo-stats`, { repos: params })
+            if (result.error) {
+                throw('Error')
+            }
+            else
+                return result
+        } catch (e) {
+            throw(e)
+        }
+    }
+
+
+    protected version: string
+    protected url: string
+    protected api
+}
+
